218123298c423a86ab0b832b0b198829b6fa6e8b
Katarina-Kovacova <ketrin11@gmail.com>
2023-12-29 21:38:34 +0000

    Complete app logic

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ locations = {
     "Cairo": {"latitude": 29.95375640, "longitude": 31.53700030},
     }
 
+
 # create function to convert_dict_to_json(locations_dict):  # function to convert cities dictionary to json file
 def cities_dict_to_json(updated_city_dict):
     with open("cities.json", "w") as f:
@@ -30,65 +31,67 @@ def load_json_to_dict():
         cities_dictionary = json.load(file)
     return cities_dictionary
 
-# def check if user town in location dict:
-# def check_if_city_in_dict(place, places_locations):  # function checks if user defined place exists in location dictionary
-#     if place in places_locations:
-#         print(f"Yes, the {place} is in our list. Let's see the weather!")
 
-# def add_city_to_dict(new_city, cities_dict):  # add new city if not in locations dictionary
-#     new_city_lat = input(f"Please enter {new_city} latitude:  ")
-#     new_city_long = input(f"Please enter {new_city} longitude:  ")
-#     new_city_lat_long = {"latitude": float(new_city_lat),
-#                          "longitude": float(new_city_long),
-#                          }
-#     cities_dict[new_city] = new_city_lat_long
-#     return cities_dict
-
-#
-print("Welcome to our weather app!")
-print()
-area = input("Please select location to view the current weather conditions: ")
+def add_city_to_dict(new_city, new_city_lat, new_city_long, cities_dict):  # add new city if not in locations dictionary
+    # new_city_lat = input(f"Please enter {new_city} latitude:  ")
+    # new_city_long = input(f"Please enter {new_city} longitude:  ")
+    new_city_lat_long = {"latitude": float(new_city_lat),
+                         "longitude": float(new_city_long),
+                         }
+    cities_dict[new_city] = new_city_lat_long
+    return cities_dict
 
 print()
 
-
-#
 
 def get_weather(town):
-    if town in locations:
-        town_lat = locations[town]["latitude"]
-        town_long = locations[town]["longitude"]
-        weather_response = requests.get(f"{api_url}&lat={town_lat}&lon={town_long}") # make call to API to receive weather data
-        town_weather = weather_response.json() # convert the API data format to json (similar to python dictionary
-        country = town_weather["sys"]["country"]
-        country_location = town_weather["name"]
-        town_current_weather_condition = town_weather["weather"][0]["description"]
-        town_current_temperature = round((town_weather["main"]["temp"]) - 273.15, 2)
-        town_min_temperature = round((town_weather["main"]["temp_min"]) - 273.15, 2)
-        town_max_temperature = round((town_weather["main"]["temp_max"]) - 273.15, 2)
-        town_wind = town_weather["wind"]["speed"]
-        town_humidity = town_weather["main"]["humidity"]
-        town_complete_data = {
-            "country:": country,
-            "location:": country_location,
-            "current_weather_condition:": town_current_weather_condition,
-            "current temperature (Celsius):": town_current_temperature,
-            "minimum temperature (Celsius)": town_min_temperature,
-            "maximum temperature (Celsius)": town_max_temperature,
-            "wind (m/s):": town_wind,
-            "humidity (%):": town_humidity,
-        }
-        for key, value in town_complete_data.items():
-            print(key, value)
-        print()
+    town_lat = locations[town]["latitude"]
+    town_long = locations[town]["longitude"]
+    weather_response = requests.get(f"{api_url}&lat={town_lat}&lon={town_long}") # make call to API to receive weather data
+    town_weather = weather_response.json() # convert the API data format to json (similar to python dictionary
+    country = town_weather["sys"]["country"]
+    country_location = town_weather["name"]
+    town_current_weather_condition = town_weather["weather"][0]["description"]
+    town_current_temperature = round((town_weather["main"]["temp"]) - 273.15, 2)
+    town_min_temperature = round((town_weather["main"]["temp_min"]) - 273.15, 2)
+    town_max_temperature = round((town_weather["main"]["temp_max"]) - 273.15, 2)
+    town_wind = town_weather["wind"]["speed"]
+    town_humidity = town_weather["main"]["humidity"]
+    town_complete_data = {
+        "country:": country,
+        "location:": country_location,
+        "current_weather_condition:": town_current_weather_condition,
+        "current temperature (Celsius):": town_current_temperature,
+        "minimum temperature (Celsius)": town_min_temperature,
+        "maximum temperature (Celsius)": town_max_temperature,
+        "wind (m/s):": town_wind,
+        "humidity (%):": town_humidity,
+    }
+    for key, value in town_complete_data.items():
+        print(key, value)
+    print()
+
+
+if __name__ == "__main__":
+
+    cities_dict_to_json(locations)
+    print("Welcome to our weather app!")
+    print()
+    print(load_json_to_dict())
+    area = input("Please select location to view the current weather conditions: ")
+    print()
+    if area not in locations:
+        print(f"Sorry this city is not on the list.\nIf you know the {area} latitude and longidute, please enter it now.")
+        area_latitude = input(f"{area} latitude:   ")
+        area_longitude = input(f"{area} longitude:   ")
+        add_city_to_dict(area, area_latitude, area_longitude, locations)
+        get_weather(area)
     else:
-        print("Ensure correct spelling or town is included in the list of locations.")
+        get_weather(area)
 
+    add_city_to_dict(area, area_latitude, area_longitude, locations)
+    cities_dict_to_json(locations)
 
-
-get_weather(area)
-cities_dict_to_json(locations)
-load_json_to_dict()
 
 
 
