4da3bc1ed76b0ff9187470d06fe6468d1802ad5e
Katarina-Kovacova <ketrin11@gmail.com>
2023-12-30 17:16:00 +0000

    Write not listed location into json file

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ api_url = os.getenv("API_URL")  # save API key to a variable api_key
 api_url = f"{api_url}?appid={api_key}"  # save API link to a variable api
 
 
-locations = {
-    "High Barnet": {"latitude": 51.650341, "longitude": -0.195190},
-    "Topolcany": {"latitude": 48.554499, "longitude": 18.179364},
-    "San Francisco": {"latitude": 37.773972, "longitude": -122.431297},
-    "Bratislava": {"latitude": 48.148598, "longitude": 17.107748},
-    "Cairo": {"latitude": 29.95375640, "longitude": 31.53700030},
-    }
+# locations = {
+#     "High Barnet": {"latitude": 51.650341, "longitude": -0.195190},
+#     "Topolcany": {"latitude": 48.554499, "longitude": 18.179364},
+#     "San Francisco": {"latitude": 37.773972, "longitude": -122.431297},
+#     "Bratislava": {"latitude": 48.148598, "longitude": 17.107748},
+#     "Cairo": {"latitude": 29.95375640, "longitude": 31.53700030},
+#     }
 
 
 # create function to convert_dict_to_json(locations_dict):  # function to convert cities dictionary to json file
-def cities_dict_to_json(updated_city_dict):
+def cities_dict_to_json(locations_dict):
     with open("cities.json", "w") as f:
-        json.dump(updated_city_dict, f, indent=4)
+        json.dump(locations_dict, f, indent=4)
 
 
 # create function to load json file into python dictionary
@@ -33,16 +33,14 @@ def load_json_to_dict():
 
 
 def add_city_to_dict(new_city, new_city_lat, new_city_long, cities_dict):  # add new city if not in locations dictionary
-    # new_city_lat = input(f"Please enter {new_city} latitude:  ")
-    # new_city_long = input(f"Please enter {new_city} longitude:  ")
     new_city_lat_long = {"latitude": float(new_city_lat),
                          "longitude": float(new_city_long),
                          }
     cities_dict[new_city] = new_city_lat_long
     return cities_dict
 
-print()
 
+print()
 
 def get_weather(town):
     town_lat = locations[town]["latitude"]
@@ -74,23 +72,24 @@ def get_weather(town):
 
 if __name__ == "__main__":
 
-    cities_dict_to_json(locations)
+    locations = load_json_to_dict()
     print("Welcome to our weather app!")
     print()
-    print(load_json_to_dict())
     area = input("Please select location to view the current weather conditions: ")
     print()
     if area not in locations:
         print(f"Sorry this city is not on the list.\nIf you know the {area} latitude and longidute, please enter it now.")
-        area_latitude = input(f"{area} latitude:   ")
-        area_longitude = input(f"{area} longitude:   ")
-        add_city_to_dict(area, area_latitude, area_longitude, locations)
+        print()
+        area_latitude = input(f"{area} latitude: ")
+        area_longitude = input(f"{area} longitude: ")
+        updated_cities_locations = add_city_to_dict(area, area_latitude, area_longitude, locations)
+        cities_dict_to_json(updated_cities_locations)
+        print()
         get_weather(area)
     else:
         get_weather(area)
 
-    add_city_to_dict(area, area_latitude, area_longitude, locations)
-    cities_dict_to_json(locations)
+
 
 
 
